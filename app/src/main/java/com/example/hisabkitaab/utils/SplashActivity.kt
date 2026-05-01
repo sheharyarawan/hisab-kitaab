cdb76cf9210ebeda94e8cea4f6526d8bfa77fe8d
Sheharyar Awan <sheharyarawan1153@gmail.com>
2026-05-01 10:19:09 +0500

    splash activity added

---
+++ app/src/main/java/com/example/hisabkitaab/utils/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.hisabkitaab.utils
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hisabkitaab.MainActivity
+import com.example.hisabkitaab.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3250)
+    }
+}
